edc78ffc06db793b277296505c49e395b786b80b
Harsh Gandhi <68757536+hkg0108@users.noreply.github.com>
2020-09-05 23:54:49 +0530

    Delete Data.js

--- Data.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("cname").innerHTML = localStorage.getItem("text1");
-document.getElementById("email").innerHTML = localStorage.getItem("text2");
-document.getElementById("phnum").innerHTML = localStorage.getItem("text3");
-document.getElementById("address").innerHTML = localStorage.getItem("text4");
